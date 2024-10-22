82177295aeeb1be7b6d698792333d2f3f3030d8c
unknown <olezio.olezio@gmail.com>
2024-10-22 16:17:27 +0300

    fix: align icons

--- dts/components.d.ts
+++ dts/components.d.ts
@@ -7,7 +7,9 @@ export {}
 /* prettier-ignore */
 declare module 'vue' {
   export interface GlobalComponents {
+    Button: typeof import('./../src/components/Button/Button.vue')['default']
     Compute: typeof import('./../src/components/Compute.vue')['default']
+    ElButton: typeof import('element-plus/es')['ElButton']
     ElConfigProvider: typeof import('element-plus/es')['ElConfigProvider']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
