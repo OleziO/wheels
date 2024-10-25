7889f45862724bae4e1e2fbbee61963880720b8a
Oleksii Riabii <o.riabiy@softonix.org>
2024-10-25 14:12:50 +0300

    feat: added supabase

--- src/api/types/schema-helpers.d.ts
+++ src/api/types/schema-helpers.d.ts
@@ -1,15 +1,7 @@
-// TODO: change the key of returning content to the one your api returns
+type TDatabase = import('./schema.types').Database['public']
 
-type TApiPaths = import('./schema').paths
+type TTableKeys = keyof TDatabase['Tables']
 
-type TPathKeys = keyof TApiPaths
+type TDBEnums = TDatabase['Enums']
 
-type TPathMethods<Path extends TPathKeys> = keyof TApiPaths[Path]
-
-type TResponse<Path extends TPathKeys, Method extends TPathMethods<Path>> = TApiPaths[Path][Method]['responses']['200']['content']['application/json; v=1.0'];
-
-type TRequestParameters<Path extends TPathKeys, Method extends TPathMethods<Path>> = TApiPaths[Path][Method]['parameters']['path']
-
-type TRequestQuery<Path extends TPathKeys, Method extends TPathMethods<Path>> = TApiPaths[Path][Method]['parameters']['query']
-
-type TRequestBody<Path extends TPathKeys, Method extends TPathMethods<Path>> = Required<TApiPaths[Path][Method]>['requestBody']['content']['application/json; v=1.0']
+type TTableModel<K extends TTableKeys> = TDatabase['Tables'][K]['Row']
